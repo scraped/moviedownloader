80e83501916fec46fa4ee5c039ec5bb399c78435
Gustavo Gama <gustavobgama@gmail.com>
2017-03-28 21:13:30 -0300

    check the watchlist every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('movie:download')
-            ->everyFiveMinutes()
+            ->everyMinute()
             ->withoutOverlapping();
 
          $schedule->command('download:check')
